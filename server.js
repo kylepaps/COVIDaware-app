518f7248b72d82770ac3ba6003e977be9c8b835e
kylepaps <kylepaps77@gmail.com>
2021-04-16 12:58:01 -0400

    deploy update

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5000
+const path = require('path')
 
 const router = require('./routes/routes')
 
 app.use('/api', router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
